e71ae87e3c2ac2767e891fa3f9002f21cd2a5713
Slayer0963 <landos112@gmail.com>
2018-05-25 19:20:40 -0600

    Terminando Codigo Neurona

--- cerebro.js
+++ cerebro.js
@@ -2,8 +2,24 @@ var SerialPort = require('serialport');
 var port = new SerialPort('COM7', {
   baudRate: 9600
 });
+var vacia;
+const brain = require('brain.js');
+var  Neurona = new brain.NeuralNetwork();
 
+   Neurona.train([
+  {input:{f:0},output:{Devil:1}},
+  {input:{f:0.09},output:{Devil:1}},
+  {input:{f:0.19},output:{Medio:1}},
+  {input:{f:0.39},output:{Medio:1}},
+  {input:{f:0.48},output:{Fuerte:1}},
+  {input:{f:1},output:{Fuerte:1}}
 
+  
+  
+  
+  ]);
+
+  //Neurona.train(datos,{log: true});
   
 
 var Sensor1 = 0;
@@ -45,7 +61,23 @@ port.on('data', function(data) {
         break;
     }
   }
-  
+  var salida = Neurona.run({f:Sensor1/1024});
+  //var fuerza = brain.likely(Sensor1/1024,Neurona);
+  //console.log("Nivel de fuerza:"+salida.Fuerte);
+
+  if(salida.Devil > 0.5){
+    console.log("Fuerza es devil");
+  }
+  else if(salida.Medio > 0.5){
+    console.log("Fuerza Media");
+  }
+  else if(salida.Fuerte > 0.8){
+    console.log("Ferza Fuerte");
+  }
+  else
+  {
+    console.log("Enclenque");
+  }
 
   
 });
